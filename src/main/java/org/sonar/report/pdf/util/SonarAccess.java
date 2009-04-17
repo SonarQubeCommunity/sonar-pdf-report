777abce3c7e23cde1566a7e29b41a87d36b086b4
Antonio Manuel Muñiz Martín <amuniz@klicap.es>
2009-04-17 21:55:51 +0000

    Adding license header

--- src/main/java/org/sonar/report/pdf/util/SonarAccess.java
+++ src/main/java/org/sonar/report/pdf/util/SonarAccess.java
@@ -1,3 +1,22 @@
+/*
+ * Sonar, open source software quality management tool.
+ * Copyright (C) 2009 SonarSource SA
+ * mailto:contact AT sonarsource DOT com
+ *
+ * Sonar is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * Sonar is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with Sonar; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
+ */
 package org.sonar.report.pdf.util;
 
 import java.io.IOException;
@@ -10,7 +29,6 @@ import org.apache.commons.httpclient.methods.GetMethod;
 import org.dom4j.Document;
 import org.dom4j.DocumentException;
 import org.dom4j.io.SAXReader;
-import org.sonar.report.pdf.mojo.SonarPDFMojo;
 
 public class SonarAccess {
   
