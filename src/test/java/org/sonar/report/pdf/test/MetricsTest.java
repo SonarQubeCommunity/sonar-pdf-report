4657d3ec4a73f4b2f53fcdc6b9a31e4ab7967cab
Antonio Manuel Muñiz Martín <amuniz@klicap.es>
2009-09-07 22:57:09 +0000

    Test config.

--- src/test/java/org/sonar/report/pdf/test/MetricsTest.java
+++ src/test/java/org/sonar/report/pdf/test/MetricsTest.java
@@ -24,7 +24,7 @@ public class MetricsTest {
     Properties configText = new Properties();
     configText.load(resourceText.openStream());
 
-    SonarAccess sonarAccess = new SonarAccess("http://nemo.sonar.codehaus.org");
+    SonarAccess sonarAccess = new SonarAccess("http://localhost:9000");
     Measures measures = new Measures();
     String allMetricsKeys = measures.getAllMetricKeys(sonarAccess);
     
