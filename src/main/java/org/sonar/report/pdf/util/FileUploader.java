d0c408c1adf9bcd2560387ec8d5984c763b5d73b
Michał Dettlaff <mdettlaff@jitsolutions.pl>
2015-06-04 18:48:43 +0200

    fix passing credentials when uploading PDF to the server

--- src/main/java/org/sonar/report/pdf/util/FileUploader.java
+++ src/main/java/org/sonar/report/pdf/util/FileUploader.java
@@ -22,8 +22,11 @@ package org.sonar.report.pdf.util;
 
 import java.io.File;
 
+import org.apache.commons.httpclient.Credentials;
 import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.HttpStatus;
+import org.apache.commons.httpclient.UsernamePasswordCredentials;
+import org.apache.commons.httpclient.auth.AuthScope;
 import org.apache.commons.httpclient.methods.PostMethod;
 import org.apache.commons.httpclient.methods.multipart.FilePart;
 import org.apache.commons.httpclient.methods.multipart.MultipartRequestEntity;
@@ -36,7 +39,7 @@ public class FileUploader {
 
   private static final Logger LOG = LoggerFactory.getLogger(PDFPostJob.class);
 
-  public static void upload(final File file, final String url) {
+  public static void upload(final File file, final String url, String username, String password) {
     PostMethod filePost = new PostMethod(url);
 
     try {
@@ -47,6 +50,11 @@ public class FileUploader {
       filePost.setRequestEntity(new MultipartRequestEntity(parts, filePost.getParams()));
 
       HttpClient client = new HttpClient();
+      if (username != null && !username.isEmpty() && password != null && !password.isEmpty()) {
+        client.getParams().setAuthenticationPreemptive(true);
+        Credentials credentials = new UsernamePasswordCredentials(username, password);
+        client.getState().setCredentials(new AuthScope(AuthScope.ANY_HOST, AuthScope.ANY_PORT), credentials);
+      }
       client.getHttpConnectionManager().getParams().setConnectionTimeout(10000);
       int status = client.executeMethod(filePost);
       if (status == HttpStatus.SC_OK) {
